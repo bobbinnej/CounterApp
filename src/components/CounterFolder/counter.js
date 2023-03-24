41586c230a0669254252abe1c3c05d1edcd69e6f
jackie <bobbinnej@gmail.com>
2023-03-24 21:49:12 +0300

    debug material ui error

--- src/components/CounterFolder/counter.js
+++ src/components/CounterFolder/counter.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import useStyles from './style';
-import { Button } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 
 const Counter = () =>{
     const classes=useStyles();
